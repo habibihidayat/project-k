56085707cabd0dde91e73ade3be58847f85396fc
Habibi hidayat <114434251+habibihidayat@users.noreply.github.com>
2025-11-15 22:53:17 +0800

    Update KeabyGUI.lua

--- KeabyGUI.lua
+++ KeabyGUI.lua
@@ -861,9 +861,9 @@ makeToggle(pnl2,"Enable Instant 2x Speed",function(on) if on then instant2x.Star
 makeSlider(pnl2,"Fishing Delay",0,5.0,0.3,function(v) instant2x.Settings.FishingDelay=v end)
 makeSlider(pnl2,"Cancel Delay",0.01,1.5,0.19,function(v) instant2x.Settings.CancelDelay=v end)
 makeSlider(pnl2,"Hook detection Delay",0.01,5.0,0.19,function(v) instant2x.Settings.HookDetectionDelay=v end)
-makeSlider(pnl2,"Max wait time",0.01,5.0,0.19,function(v) instant2x.Settings.MaxWaitTime=v end)
-makeSlider(pnl2,"Fallback Time",0.01,5.0,0.19,function(v) instant2x.Settings.FallbackTime=v end)
-makeSlider(pnl2,"Hook Stagger",0.01,5.0,0.15,function(v) instant2x.Settings.HookStagger=v end)
+makeSlider(pnl2,"Max wait time",0.01,5.0,0.19,function(v) instant2x.Settings.MaxWaitTim=v end)
+makeSlider(pnl2,"Fallback Time",0.01,5.0,0.19,function(v) instant2x.Settings.RetryDelay=v end)
+
 
 
 
@@ -1310,6 +1310,7 @@ print("✨ Keaby GUI v4.0 Ultra MOBILE OPTIMIZED loaded!")
 print("📱 Perfect for mobile devices")
 print("🔧 Smaller UI, dropdown teleport system")
 print("💎 Created by Keaby Team")
+
 
 
 
