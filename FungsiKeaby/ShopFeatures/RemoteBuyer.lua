b1a6bd2568782b9e498646d02fcf6f38ea22b4a3
Habibi hidayat <114434251+habibihidayat@users.noreply.github.com>
2025-12-01 15:43:13 +0800

    Update RemoteBuyer.lua

--- FungsiKeaby/ShopFeatures/RemoteBuyer.lua
+++ FungsiKeaby/ShopFeatures/RemoteBuyer.lua
@@ -1,7 +1,12 @@
-RemoteBuyer = {}
+local RemoteBuyer = {}
 
 local RS = game:GetService("ReplicatedStorage")
-local PurchaseRodRemote = RS.Packages["_Index"]["sleitnick_net@0.2.0"].net["RF/PurchaseFishingRod"]
+
+-- ==========================================
+-- Remote untuk membeli ROD
+-- ==========================================
+local PurchaseRodRemote =
+    RS.Packages["_Index"]["sleitnick_net@0.2.0"].net["RF/PurchaseFishingRod"]
 
 function RemoteBuyer.BuyRod(id)
     task.spawn(function()
@@ -13,6 +18,26 @@ function RemoteBuyer.BuyRod(id)
             Notify.Send("Buy Rod", "Berhasil membeli rod!", 3)
         else
             Notify.Send("Error", "Gagal membeli rod!", 3)
+        end
+    end)
+end
+
+-- ==========================================
+-- Remote untuk membeli BAIT
+-- ==========================================
+local PurchaseBaitRemote =
+    RS.Packages["_Index"]["sleitnick_net@0.2.0"].net["RF/PurchaseBait"]
+
+function RemoteBuyer.BuyBait(id)
+    task.spawn(function()
+        local success, result = pcall(function()
+            return PurchaseBaitRemote:InvokeServer(id)
+        end)
+
+        if success then
+            Notify.Send("Buy Bait", "Berhasil membeli bait!", 3)
+        else
+            Notify.Send("Error", "Gagal membeli bait!", 3)
         end
     end)
 end
